bcb153e092f3f189681801e9beabe230fb7a2304
omprakashpandit <32616762+omprakashpandit@users.noreply.github.com>
2024-07-04 22:15:28 +0530

    MongoDBConnectionTest

---
+++ MongoDBTest.java
@@ -0,0 +1,31 @@
+package com.pak;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoDBTest {
+
+	public static void main(String[] args) {
+		// Mongodb compass:-(Local)
+		// MongoClient mclient = new MongoClient("localhost", 27017);
+		// Atlas:- (Cloud)
+		MongoClientURI uri = new MongoClientURI(
+				"mongodb+srv://omprakash:1234@cluster0.z5oxtrc.mongodb.net/zap?retryWrites=true&w=majority&appName=Cluster0");
+		MongoClient mclient = new MongoClient(uri);
+		MongoDatabase db = mclient.getDatabase("populations");
+		MongoCollection<Document> col = db.getCollection("cities");
+		FindIterable<Document> doc = col.find();
+		System.out.println("connection successfully.!");
+		for (Document d : doc) {
+			System.out.println("Name: " + d.get("name") + " " + "Country: " + d.get("country") + " " + "Population: "
+					+ d.get("population"));
+
+		}
+	}
+}
